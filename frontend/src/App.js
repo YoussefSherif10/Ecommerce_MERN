7d082d7824c15b723fa5d5ce584c05fc11ecb8c8
yousseffekry <mr-youssefsherif@hotmail.com>
2023-05-09 22:04:31 +0300

    create login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import {useForm} from "react-hook-form";
+
+const API_ENDPOINT = 'http://localhost:3000/'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {register, handleSubmit} = useForm();
+    const [loginData, setLoginData] = useState(null)
+    console.log({loginData})
+
+    const postData = async (postedData) => {
+        const data = await fetch(`${API_ENDPOINT}/users/login`, {
+            method: 'POST', body: JSON.stringify(postedData)
+        })
+        const jsonData = await data.json();
+        return jsonData;
+    }
+
+    const onSubmitfunc = (data) => {
+        console.log({data})
+        const token = postData(data)
+        setLoginData(token)
+    }
+    return (<div style={{display: 'flex', flexDirection: 'column', width: 'fit-content', height: '100%'}}>
+            <form onSubmit={handleSubmit(onSubmitfunc)}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                           placeholder="Enter email" {...register('mail')}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"
+                           placeholder="Password" {...register('password')}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
